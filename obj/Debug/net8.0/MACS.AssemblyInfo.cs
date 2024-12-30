345d65855896a558596c750f277dbc0c623a5de7
hiepbao <cubi27395@gmail.com>
2024-12-30 15:11:12 +0700

    #

--- obj/Debug/net8.0/MACS.AssemblyInfo.cs
+++ obj/Debug/net8.0/MACS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MACS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfdc8eb12580d0791d23d9d1afd9ba82494da719")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b3e0983bcf731a15c4ad23417a30478d803451")]
 [assembly: System.Reflection.AssemblyProductAttribute("MACS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MACS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
